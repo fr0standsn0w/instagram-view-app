1f036a55dcf155244c711f5f7a765e73fd87cac3
Галкин Владислав <vladislav_galkin@f-lab.tech>
2023-03-17 18:03:29 +0500

    Edited with new design

--- src/pages/profile/profileInfo/profileInfo.tsx
+++ src/pages/profile/profileInfo/profileInfo.tsx
@@ -4,22 +4,43 @@ import fav from '../../../asstets/img/favicon.jpg'
 
 const ProfileInfo = () => {
     return (
-        <div className={style.profileInfoContainer}>
-            <div className={style.profilePicContainer}>
-                <div className={style.profilePic}>
-                    <img src={`${fav}`} alt=""/>
+            <div className={style.profileInfoContainer}>
+                <p className={style.browsing}>Anonymous browsing</p>
+                <div className={style.profileInfoContent}>
+                    <div className={style.firstColInfoContainer}>
+                        <div className={style.profilePicContainer}>
+                            <div className={style.profilePic}>
+                                <img src={`${fav}`} alt=""/>
+                            </div>
+                        </div>
+                        <div className={style.mainInfoContent}>
+                            <h4>@sinitovskiy1</h4>
+                            <p>поль</p>
+                        </div>
+                    </div>
+
+                    <div className={style.mainInfo}>
+                        {/*<div className={style.mainInfoContent}>
+                            <h4>@sinitovskiy1</h4>
+                            <p>поль</p>
+                        </div>*/}
+                        <div className={style.status}>
+                            <div className={style.infoElement}>
+                                <p>6</p>
+                                <p>publications</p>
+                            </div>
+                            <div className={style.infoElement}>
+                                <p>121</p>
+                                <p>followers</p>
+                            </div>
+                            <div className={style.infoElement}>
+                                <p>121</p>
+                                <p>follow</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className={style.mainInfo}>
-                <div className={style.mainInfoContent}>
-                    <h4>@sinitovskiy1</h4>
-                    <p>поль</p>
-                </div>
-                <div className={style.status}>
-                    Anonymous browsing
-                </div>
-            </div>
-        </div>
     );
 };
 
